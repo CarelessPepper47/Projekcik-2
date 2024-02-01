749bd6e46540f52595926de64737b4be4ae64ea2
MatiDev <mtsz.raszeja@gmail.com>
2024-02-01 16:55:49 +0100

    yaaaz

---
+++ Strona2.js
@@ -0,0 +1,17 @@
+// prompt, po przecinku jest placeholder "100" w tym przypadku
+
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+// confirm 
+
+let isAdult = confirm("Are you +18 YO?");
+
+alert( isAdult ); // true if OK is pressed
+
+// ZADANKO 
+
+const imie = prompt('Jak masz dupe?', "Kupa")
+
+alert(`Masz na imię ${imie}`);
--- index.js
+++ index.js
@@ -5,15 +5,15 @@ console.log("kupa");
 let message1;
 message1 = "Hello";
 
-alert(message1);
+// alert(message1);
 
-// bardziej zaawansowane, można w jednej linijce
+// ///////////////////bardziej zaawansowane, można w jednej linijce
 
 let message2 = "Hello";
 
-alert(message2);
+// alert(message2);
 
-// w jednej linijce, mniej przejżyście
+// ///////////////////w jednej linijce, mniej przejżyście
 
 let user3 = 'John', age3 = 25, message3 = 'Hello';
 
@@ -25,3 +25,61 @@ let message4 = 'Hello';
 
 // let a const -> let jest zmienna którą możemy z czasem NADPISAĆ, a const jest nietykalne
 
+const nameAdmin = "John";
+const admin = nameAdmin;
+
+alert(admin)
+
+const nazwaPlanety = "Ziemia";
+const currentUserName = "John";
+
+const BIRTHDAY = "18.04.1982";
+const age = someCode(BIRTHDAY);
+
+// Data Types
+
+let str = "Hello"; // Strings
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+let n = 123; // Numbers
+n = 12.345;
+
+let nameFieldChecked = true; // Boolean
+let ageFieldChecked = false;
+
+let age5 = null; // null
+
+let age99; // undefined
+
+alert(age99); // shows "undefined"
+
+///////////////////// TYPEOF <<<< sprawdzanie co czym jest
+
+typeof undefined // "undefined"
+
+typeof 0 // "number"
+
+typeof 10n // "bigint"
+
+typeof true // "boolean"
+
+typeof "foo" // "string"
+
+typeof Symbol("id") // "symbol"
+
+typeof Math // "object"  (1)
+
+typeof null // "object"  (2)
+
+typeof alert // "function"  (3)
+
+// Zadanie
+
+let nameZad = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${nameZad}` ); // hello Ilya
