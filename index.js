8b0b46ecb86b8416e3fef9d79743e312e25ece4a
MatiDev <mtsz.raszeja@gmail.com>
2024-02-01 15:55:06 +0100

    pierwszy commicik w nowym repozytorium

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("kupa");
+
+
+// podstawka - zapisane nieco dłużej
+let message1;
+message1 = "Hello";
+
+alert(message1);
+
+// bardziej zaawansowane, można w jednej linijce
+
+let message2 = "Hello";
+
+alert(message2);
+
+// w jednej linijce, mniej przejżyście
+
+let user3 = 'John', age3 = 25, message3 = 'Hello';
+
+// lub w wielu
+
+let user4 = 'John';
+let age4 = 25;
+let message4 = 'Hello';
+
+// let a const -> let jest zmienna którą możemy z czasem NADPISAĆ, a const jest nietykalne
+
